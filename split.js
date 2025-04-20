ea13b076ec457a0f9ed8dab96417685e0e498f45
kyzentyven <kyzentyven@gmail.com>
2025-04-20 23:47:42 +0600

    Added the basic concepts of split in JavaScript.

---
+++ split.js
@@ -0,0 +1,8 @@
+const sentence = 'I am a good and hardworking person.';
+
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+
+const friendsStr = 'Rahim,Karim,Jabbar,Abdul';
+const friends = friendsStr.split(',');
+console.log(friends);
