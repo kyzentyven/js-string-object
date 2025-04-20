33fa8f41f8eb2d8509c8793d98b4c02e696d2ec4
kyzentyven <kyzentyven@gmail.com>
2025-04-20 23:55:02 +0600

    Added the basic concepts of concat in JavaScript.

---
+++ concat.js
@@ -0,0 +1,8 @@
+const first = 'Kamrujjaman';
+const last = 'Tuhin';
+
+const fullName = first + ' ' + last;
+console.log(fullName);
+
+const anotherFullName = first.concat(' ').concat(last);
+console.log(anotherFullName);
