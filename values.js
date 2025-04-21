d1bc3834d846594f02e6bb5b820625ea3dadebef
kyzentyven <kyzentyven@gmail.com>
2025-04-21 23:19:24 +0600

    Added the basic concepts of object values in JavaScript.

---
+++ values.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand: 'Lenovo',
+    price: '70000',
+    processor: 'AMD',
+    ssd: '512 GB',
+    display: 'OLED'
+}
+
+const values = Object.values(computer);
+console.log(values);
