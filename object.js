c69f9782a449e4182a24a8dfe6f3d18dfdf63b0e
kyzentyven <kyzentyven@gmail.com>
2025-04-21 21:46:54 +0600

    Added the basic concepts of object in JavaScript.

---
+++ object.js
@@ -0,0 +1,35 @@
+// Object type: Primitive
+const age = 22;
+const school = 'United Islamia Govt. High School';
+const isPassed = true;
+
+let isDeveloper;
+console.log(isDeveloper);
+
+const subjects = ['Physics', 'Chemisrty', 'Mathematics'];
+
+const bottleWrong = ['White', 45];
+const bottleColor = 'White';
+const bottlePrice = 45;
+const bottleDrinks = 'Water';
+
+// Object declaration
+// Object type: Non-Primitive
+const bottle = {
+    brand: 'RFL',
+    price: 100,
+    color: 'White',
+    isClean: true
+}
+
+// Another object declaration. Object type: Non-Primitive
+const subject = {
+    name: 'OS',
+    teacher: 'Faisal Ahmed',
+    examDate: '24 April',
+    chapters: ['Fourth', 'Sixth', 'Seventh'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
