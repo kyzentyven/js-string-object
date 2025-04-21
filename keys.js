9eb3ffacb1afc4635d39b7b46b17ee964c11b1c2
kyzentyven <kyzentyven@gmail.com>
2025-04-21 23:17:33 +0600

    Added the basic concepts of object keys in JavaScript.

---
+++ keys.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand: 'Lenovo',
+    price: '70000',
+    processor: 'AMD',
+    ssd: '512 GB',
+    display: 'OLED'
+}
+
+const keys = Object.keys(computer);
+console.log(keys);
